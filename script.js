23e0e59dedd01e1fa7d3c68a352aeeec9a4d6048
Luiza Alencar <66708154+luizadealencar@users.noreply.github.com>
2024-05-12 16:54:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+  // Seleciona todos os elementos li com a classe "dropdown"
+  var dropdowns = document.querySelectorAll('.dropdown');
+
+  // Itera sobre cada dropdown
+  dropdowns.forEach(function(dropdown) {
+    // Seleciona o dropdown-content associado a este dropdown
+    var dropdownContent = dropdown.querySelector('.dropdown-content');
+
+    // Adiciona um evento de clique a cada dropdown
+    dropdown.addEventListener('click', function(event) {
+      // Verifica se o dropdown-content está visível
+      if (dropdownContent.style.display === 'none' || dropdownContent.style.display === '') {
+        // Se estiver oculto, mostra-o
+        dropdownContent.style.display = 'block';
+      } else {
+        // Se estiver visível, oculta-o
+        dropdownContent.style.display = 'none';
+      }
+
+      // Evita que o clique no dropdown siga o link padrão apenas se o alvo do clique não for um link
+      if (!event.target.closest('a')) {
+        event.preventDefault();
+      }
+    });
+  });
