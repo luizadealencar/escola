0f0108e43ad9ccbe9abd12ffaba96909a56c95d0
Luiza Alencar <66708154+luizadealencar@users.noreply.github.com>
2024-05-20 08:45:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
-// Seleciona todos os elementos li com a classe "dropdown"
 var dropdowns = document.querySelectorAll('.dropdown');
 
-// Itera sobre cada dropdown
 dropdowns.forEach(function (dropdown) {
-  // Seleciona o dropdown-content associado a este dropdown
   var dropdownContent = dropdown.querySelector('.dropdown-content');
+  var dropdownContent = dropdown.querySelector('.dropdown-content dropdown-submenu-content');
 
-  // Adiciona um evento de clique a cada dropdown
   dropdown.addEventListener('click', function (event) {
-    // Verifica se o dropdown-content está visível
     if (dropdownContent.style.display === 'none' || dropdownContent.style.display === '') {
-      // Se estiver oculto, mostra-o
       dropdownContent.style.display = 'block';
     } else {
-      // Se estiver visível, oculta-o
       dropdownContent.style.display = 'none';
     }
 
-    // Evita que o clique no dropdown siga o link padrão apenas se o alvo do clique não for um link
     if (!event.target.closest('a')) {
       event.preventDefault();
     }
   });
 });
 
+function setActiveSection() {
+  const sections = document.querySelectorAll(".syntax, .example, .reads");
+  sections.forEach((section) => {
+    const bounding = section.getBoundingClientRect();
+    if (
+      bounding.top >= 0 &&
+      bounding.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight)
+    ) {
+      section.classList.add("active-section");
+    } else {
+      section.classList.remove("active-section");
+    }
+  });
+}
 
+window.addEventListener("scroll", setActiveSection);
+window.addEventListener("load", setActiveSection);
